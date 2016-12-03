a301978a03a4a45e5b4368a5ca5313671da891c5
Evgeny Poberezkin <e.poberezkin@me.com>
2016-12-03 18:38:06 +0000

    test: async schemas (Ajv specific)

--- spec/test.fail.js
+++ spec/test.fail.js
@@ -3,11 +3,26 @@
 var jsonSchemaTest = require('..');
 var Ajv = require('ajv');
 var ajv = new Ajv;
+var assert = require('assert');
+
+describe('failing tests', () => {
+  var hookCalled;
+
+  beforeEach(() => hookCalled = undefined);
+
+  jsonSchemaTest(ajv, {
+    description: 'failing tests',
+    suites: { 'tests': './tests/*.fail.json' },
+    cwd: __dirname,
+    afterError: () => hookCalled = true
+  });
+
+  afterEach(() => assert(hookCalled));
+});
 
 jsonSchemaTest(ajv, {
-  description: 'failing tests',
-  suites: {
-    'tests': './tests/*.fail.json'
-  },
+  description: 'failing async tests',
+  async: true,
+  suites: { 'tests': './tests/*.fail.async.json' },
   cwd: __dirname
 });
--- spec/test.pass.js
+++ spec/test.pass.js
@@ -5,10 +5,38 @@ var Ajv = require('ajv');
 var ajv = new Ajv;
 var assert = require('assert');
 
+describe('passing tests', () => {
+  var hookCalled;
+
+  beforeEach(() => hookCalled = undefined);
+
+  jsonSchemaTest(ajv, {
+    description: 'passing tests',
+    suites: { 'tests': './tests/*.pass.json' },
+    cwd: __dirname,
+    afterEach: () => hookCalled = true
+  });
+
+  jsonSchemaTest([ajv, ajv], {
+    description: 'passing tests, two validators',
+    suites: { 'tests': './tests/*.pass.json' },
+    cwd: __dirname,
+    afterEach: () => hookCalled = true
+  });
+
+  afterEach(() => assert(hookCalled));
+});
+
 jsonSchemaTest(ajv, {
-  description: 'passing tests',
-  suites: {
-    'tests': './tests/*.pass.json'
-  },
+  description: 'passing async tests',
+  async: true,
+  suites: { 'tests': './tests/*.pass.async.json' },
+  cwd: __dirname
+});
+
+jsonSchemaTest([ajv, ajv], {
+  description: 'passing async tests, two validators',
+  async: true,
+  suites: { 'tests': './tests/*.pass.async.json' },
   cwd: __dirname
 });
